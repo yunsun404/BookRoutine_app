e04a2113133c5a9b508bafa58148d04d445ccaf0
hayd12-lab <hayoungi825@gmail.com>
2026-08-20 14:16:12 +0900

    chore: 개발 환경 WSL에서 Windows 네이티브로 전환

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
+import ws from "ws";
 
 const supabaseUrl = "https://vdzyacisqouvpsxuqltt.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkenlhY2lzcW91dnBzeHVxbHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMTI5MDEsImV4cCI6MjA5Mzg4ODkwMX0.Nf-9mSWWUGNfCxbJrcnEYxUyJuNfn_k0B5-7s9O4rCo";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  realtime: {
+    transport: ws as any, // 여기 realtime 옵션 안에 넣어야 함
+  },
+});
